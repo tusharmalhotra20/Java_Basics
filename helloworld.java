f000e9d1cb28323d7467a60bb4ee183acdef4634
Tushar099099 <42891196+Tushar099099@users.noreply.github.com>
2019-07-04 13:35:46 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+package com.edureka;
+
+public class helloworld { //name of class created as "hello world"
+
+    public static void main (String [] args) { //return type is void that is nothing is returned
+
+      System.out.println("Hello world..!!");  //print statement
+
+    }
+}
